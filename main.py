7041dc603e967e217f9dc8b9089893da06e23ca6
Tejas H <tvh1904@gmail.com>
2026-04-26 19:01:09 +0100

    Added threading

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 import cv2
+import threading
 from ultralytics import YOLO
+
+def object_tracking(model, wanted_items, cap):
+        while True:
+            ret, frame = cap.read() #Captures the frames 
+
+            #If a frame is not returned
+            if not ret:
+                print("Unable to recieve the frame")
+                break
+
+            #The results of using the YOLO model to track objects in the frame 
+            results = model.track(frame, verbose=False) 
+
+            #Loops through results to see if the detected object is one of the wanted items 
+            for r in results[0].boxes:
+                if model.names[r.cls.item()] in wanted_items:
+                    print("Wanted item detected: ", model.names[r.cls.item()])
+                
+
+            annotated_frame = results[0].plot() #Gets the list the labels and draws bounding boxes on the objects. 
+
+            cv2.imshow('Webcam Feed', annotated_frame) #Displays the webcam feed
+
+            #Ends the camera feed if 'q' is pressed
+            if cv2.waitKey(1) == ord('q'):
+                print("Exiting...")
+                break 
 
 #Load Yolov8
 model = YOLO("yolov8n.pt")
@@ -14,31 +42,18 @@ if not cap.isOpened():
     print("Unable to open camera.")
 
 try:
-    while True:
-        ret, frame = cap.read() #Captures the frames 
+    #Start the object tracking loop in a background thread
+    threads = []
+    threads.append(threading.Thread(target=object_tracking, args=(model, wanted_items, cap), daemon=True)) #daemon=True means that the thread dies when the program exits
+    
+    # Start each thread
+    for t in threads:
+        t.start()
 
-        #If a frame is not returned
-        if not ret:
-            print("Unable to recieve the frame")
-            break
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
 
-        results = model.track(frame, verbose=False) 
-
-        #Loops through results to see if the detected object is one of the wanted items 
-        for r in results[0].boxes:
-            if model.names[r.cls.item()] in wanted_items:
-                print("Wanted item detected: ", model.names[r.cls.item()])
-            
-
-        annotated_frame = results[0].plot() #Gets the list the labels and draws bounding boxes on the objects. 
-
-        cv2.imshow('Webcam Feed', annotated_frame) #Displays the webcam feed
-
-        #Ends the camera feed if 'q' is pressed
-        if cv2.waitKey(1) == ord('q'):
-            print("Exiting...")
-            break 
-        
 finally:
     #Ends the camera feed 
     cap.release()
