fdb895f7e1ec67ecc95751b0d95ee5bd27394fa8
Tejas H <tvh1904@gmail.com>
2026-05-11 19:44:29 +0100

    Added a queue which recieves the detected data

--- main.py
+++ main.py
@@ -21,8 +21,10 @@ def object_tracking(model, wanted_items, cap):
             for r in results[0].boxes:
                 if model.names[r.cls.item()] in wanted_items:
                     print("Wanted item detected: ", model.names[r.cls.item()])
+                    detection_data = {"class":model.names[r.cls.item()], "id":r.id.item(), "confidence":r.conf.item()} #Make a dictionary for the queue
+                    detection_queue.put(detection_data) #Add it into the Queue
 
-            annotated_frame = results[0].plot() #Gets the list the labels and draws bounding boxes on the objects. 
+            annotated_frame = results[0].plot() #Gets the list the labels and draws bounding boxes on the objects
 
             cv2.imshow('Webcam Feed', annotated_frame) #Displays the webcam feed
 
@@ -34,7 +36,7 @@ def object_tracking(model, wanted_items, cap):
 #Load Yolov8
 model = YOLO("yolov8n.pt")
 
-#A list of all the items  and ids wanted to be detected. The items will later be converted into ids. 
+#A list of all the items  and ids wanted to be detected. The items will later be converted into ids.
 wanted_items = ["person", "car", "truck", "dog"]
 wanted_ids = []
 
@@ -42,6 +44,9 @@ wanted_ids = []
 for id, name in model.names.items():
     if name in wanted_items:
         wanted_ids.append(id)
+
+#Make a queue
+detection_queue = queue.Queue()
 
 #Setting up the flask app
 app = Flask(__name__)
@@ -63,14 +68,14 @@ try:
     threads = []
     threads.append(threading.Thread(target=object_tracking, args=(model, wanted_items, cap), daemon=True)) #daemon=True means that the thread dies when the program exits
     
-    # Start each thread
+    #Start each thread
     for t in threads:
         t.start()
 
     #Run the Flask app
     socketio.run(app, debug=False, use_reloader=False)
 
-    # Wait for all threads to finish
+    #Wait for all threads to finish
     for t in threads:
         t.join()
     
